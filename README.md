c22da50b2cd1f4a2bb0233866fddba8e856ab2ad
Darkmatmat <mateo.leonard25@gmail.com>
2024-12-11 16:21:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com
+ Evaluation Java
---
+++ src/test/java/Evaluation/Java/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package Evaluation.Java.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
